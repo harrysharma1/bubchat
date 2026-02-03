6759e231f6b04605420dd6c4162c1800dd505936
Harry Sharma <harrysharma1066@gmail.com>
2026-02-03 19:38:45 +0000

    feat: add hub for clients to register, unregister and broadcasting

--- server/hub.go
+++ server/hub.go
@@ -6,3 +6,45 @@ type Hub struct {
 	register   chan *Client
 	unregister chan *Client
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan Message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+
+			message := Message{
+				Type:     "welcome",
+				Value:    "joined chat",
+				Username: client.username,
+				UserId:   client.userId,
+			}
+
+			for client := range h.clients {
+				client.send <- message
+			}
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				client.send <- message
+			}
+		}
+
+	}
+}
