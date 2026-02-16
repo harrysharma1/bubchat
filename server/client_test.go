dbb1467fff4a7efbfa9056398ad5296b27e1b7d5
Harry Sharma <harrysharma1066@gmail.com>
2026-02-16 14:32:20 +0000

    feat: add servews register client test

--- server/client_test.go
+++ server/client_test.go
@@ -1,7 +1,38 @@
 package server
 
-import "testing"
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-func TestRegisterClient(t *testing.T) {
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+)
 
+func TestServeWS_RegisterClient(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	hub := NewHub()
+	go hub.Run(ctx)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ServeWS(hub, w, r)
+	}))
+	defer server.Close()
+
+	wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws?username=testuser"
+
+	conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	var msg Message
+	err = conn.ReadJSON(&msg)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "welcome", msg.Type)
+	assert.Equal(t, "joined chat", msg.Value)
+	assert.Equal(t, "testuser", msg.Username)
 }
